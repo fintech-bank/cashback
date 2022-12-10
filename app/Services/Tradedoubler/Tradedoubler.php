27a7dde4df54b585ab8c4a277ead0d402d5f5b68
mmockelyn <trainznation@gmail.com>
2022-12-10 20:03:56 +0100

    Feat Init System

--- app/Services/Tradedoubler/Tradedoubler.php
+++ app/Services/Tradedoubler/Tradedoubler.php
@@ -24,7 +24,7 @@ class Tradedoubler
             'grant_type' => 'password',
             'username' => 'resabiletcse.com',
             'password' => 'CRIDIP85100'
-        ])->body();
+        ])->status();
 
         return $this->access_token;
     }
