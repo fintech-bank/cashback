3d74c8a35677b5e986bd5f6e8844f46ccbb7e0c5
mmockelyn <trainznation@gmail.com>
2022-12-10 20:06:53 +0100

    Feat Init System

--- app/Services/Tradedoubler/Tradedoubler.php
+++ app/Services/Tradedoubler/Tradedoubler.php
@@ -20,10 +20,30 @@ class Tradedoubler
      */
     public function setAccessToken(): string
     {
-        return \Http::withToken($this->bearer_encode, 'Basic')->post($this->endpoint.'uaa/oauth/token', [
-            'grant_type' => 'password',
-            'username' => 'resabiletcse.com',
-            'password' => 'CRIDIP85100'
-        ])->throw()->body();
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://connect.tradedoubler.com/uaa/oauth/token");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        $fields = <<<EOT
+grant_type=password&username=resabiletcse.com&password=CRIDIP85100
+
+EOT;
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "Authorization: Basic MTJkMjM3ZjgtNjYwZC0zZTM0LTlhMTItOTcwZDRiOTZiMzhjOjI2MTA1ZTRjYjM0OTA2Njg="
+        ));
+
+        $response = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+
+        var_dump($info["http_code"]);
+        return $response;
     }
 }
